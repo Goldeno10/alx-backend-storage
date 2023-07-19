2520add5548d2c21ecc73b10c9f1b21a2c682cb4
Goldneo10 <ibrahimmuhammad271@gmail.com>
2023-07-19 19:28:46 +0100

    Created the cash class

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -30,7 +30,7 @@ Task:
 
 
 import redis
-from typing import Union, Callable
+from typing import Any, Awaitable, Union, Callable
 import uuid
 
 
@@ -40,20 +40,41 @@ class Cache:
     """
     def __init__(self):
         """This method Initializes this object"""
-        _redis = redis.Redis()
-        _redis.flushdb
+        self._redis = redis.Redis()
+        self._redis.flushdb
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Generates and return a random key using the uuid module"""
-        key: str = uuid.uuid4()
+        key: str = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Callable = None):
+    def get(self, key: str, fn: Callable = None) -> Union[Awaitable, Any]:
         """
         method that take a key string argument and an optional Callable
         argument named fn. This callable will be used to convert the data
         back to the desired format.
         """
-        data = self._redis.get(key) if not fn else fn(self._redis.get(key))
+        data: Union[Awaitable, Any] = self._redis.get(key) if not fn else fn(self._redis.get(key))
         return data
+
+    def get_str(self, key: str) -> str:
+        """convert the data back to the str format."""
+        return self.get(key, lambda value: value.decode('utf8'))
+
+    def get_int(self, key: str) -> int:
+        """ convert the data back to the integer format."""
+        return self.get(key, lambda value: int(value))
+
+
+cache = Cache()
+
+TEST_CASES = {
+    b"foo": None,
+    123: int,
+    "bar": lambda d: d.decode("utf-8")
+}
+
+for value, fn in TEST_CASES.items():
+    key = cache.store(value)
+    assert cache.get(key, fn=fn) == value
