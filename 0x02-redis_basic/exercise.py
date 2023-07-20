b41b294ac82e733886c609bbbd7d18ab0f38b4e0
Goldneo10 <ibrahimmuhammad271@gmail.com>
2023-07-20 16:25:48 +0100

    Completed task 4

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -35,6 +35,30 @@ from typing import Any, Awaitable, Union, Callable
 import uuid
 
 
+def replay(method: Callable) -> None:
+    """
+    This function displays the history of calls of a particular
+    function.
+    """
+    method_name = method.__qualname__
+    inputs_param = f"{method_name}:inputs"
+    outputs_param = f"{method_name}:outputs"
+
+    # Connect to Redis
+    r = redis.Redis()
+
+    # Get the list of inputs and outputs from Redis
+    inputs = r.lrange(inputs_param, 0, -1)
+    outputs = r.lrange(outputs_param, 0, -1)
+
+    # Zip the inputs and outputs together and print the replay information
+    print(f"{method_name} was called {len(inputs)} times:")
+    for inp, outp in zip(inputs, outputs):
+        input_str = inp.decode('utf-8')
+        output_str = outp.decode('utf-8')
+        print(f"{method_name}(*({input_str},)) -> {output_str}")
+
+
 def count_calls(method: Callable) -> Callable:
     """ decorator that takes a single method Callable
     argument and returns a Callable.
