441f6ac74f95b6301c5bba44d1a9152399d2294b
Goldneo10 <ibrahimmuhammad271@gmail.com>
2023-09-23 20:56:58 +0100

    ..

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,4 +1,4 @@
 -- Title: Index on name and score
 -- Desc: Create an index on the first letter of the
 -- name column and the score column
-CREATE INDEX idx_name_first_score ON names (LEFT(name, 1), score);
+CREATE INDEX idx_name_first_score ON names (name(1), score);
