aaf631becad8ebf0bcaa65b0ef39fb61f4086707
Goldneo10 <ibrahimmuhammad271@gmail.com>
2023-09-23 16:40:53 +0100

    initial commit with tasks 0,1, and 2

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,29 @@
+-- Write a SQL script that ranks country origins of bands,
+-- ordered by the number of (non-unique) fans
+
+-- Requirements:
+
+-- Import this table dump: metal_bands.sql.zip
+-- Column names must be: origin and nb_fans
+-- Your script can be executed on any database
+-- Context: Calculate/compute something is always power intensive…
+--  better to distribute the load!
+--  (especially in the case of a very large table)
+-- Create a temporary table to hold the ranks
+CREATE TEMPORARY TABLE temp_ranked_origins AS
+SELECT
+    origin,
+    DENSE_RANK() OVER (ORDER BY SUM(nb_fans) DESC) AS country_rank
+FROM
+    metal_bands
+GROUP BY
+    origin;
+
+-- Retrieve the ranked country origins
+SELECT
+    origin,
+    country_rank
+FROM
+    temp_ranked_origins
+ORDER BY
+    country_rank;
